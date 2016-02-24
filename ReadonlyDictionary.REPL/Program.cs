d4ee43375aae451461f1002c288e5cccacb19733
Steven Dawkins <steven.dawkins@gmail.com>
2016-02-24 14:31:35 +0000

    Added Replify REPL

---
+++ ReadonlyDictionary.REPL/Program.cs
@@ -0,0 +1,26 @@
+﻿using ReadOnlyDictionary;
+using ReadOnlyDictionaryTests;
+using ReadOnlyDictionaryTests.SampleData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadonlyDictionary.REPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repl = new Replify.ClearScriptRepl();
+
+            var randomData = RandomDataGenerator.RandomData(100000).ToArray();
+            var storage = new InMemoryKeyValueStorage<Book>(randomData);
+
+            repl.AddHostObject("storage", storage);
+
+            repl.StartReplLoop();
+        }
+    }
+}
